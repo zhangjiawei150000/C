d422274194c8571ca385b8281584517b4cf34830
zhangjiawei <zhangjiawei150000@163.com>
2017-10-23 14:22:58 +0800

    //  C_01_02_HelloWorld //  C_01_04_Calculator

---
+++ C_01_02_HelloWorld/C_01_02_HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_01_02_HelloWorld
+//
+//  Created by zhangjiawei on 2017/10/23.
+//  Copyright © 2017年 zhangjiawei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    
+    return 0;
+}
---
+++ C_01_04_Calculator/C_01_04_Calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_01_04_Calculator
+//
+//  Created by zhangjiawei on 2017/10/23.
+//  Copyright © 2017年 zhangjiawei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("12 + 34 = %d\n", 12 + 34);
+    
+    return 0;
+}
